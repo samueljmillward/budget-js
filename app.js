55243c4ac7ab028d0afd003f349bae455f5abaa5
samueljmillward <samuel.j.pearce@hotmail.com>
2020-08-19 19:34:01 +0100

    Adds items to budget controller

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expense__list'
     };
 
     return {
@@ -72,11 +74,30 @@ var UIController = (function () {
             };
         },
 
+        addListItem: function (obj, type) {
+            var html, newHtml, element;
+
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+
+                html = '<div class="item clearfix" id="exp-%id%"> <div class="item__description">%description% </div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description)
+            newHtml = newHtml.replace('%value%', obj.value)
+
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
-    };
-
+    }
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
@@ -99,6 +120,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         input = UICtrl.getInput();
 
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+        UICtrl.addListItem(newItem, input.type);
     };
 
     return {
