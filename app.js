01980dcf17b61cd8f8b4b1dc8aa7b3375300944a
samueljmillward <samuel.j.pearce@hotmail.com>
2020-08-25 19:43:28 +0100

    Displays correct date in UI

--- app.js
+++ app.js
@@ -248,6 +248,18 @@ var UIController = (function () {
             });
         },
 
+        displayMonth: function () {
+            var now, month, months, year;
+
+            now = new Date();
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -328,6 +340,8 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     return {
         init: function () {
+            UICtrl.displayMonth();
+
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
